62ad42cb9071978b671bf27bae2fa2818149a39b
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-01-07 11:03:41 +0900

    1장. 오브젝트와 의존관계

---
+++ README.md
@@ -0,0 +1,4 @@
+# 토비의 스프링
+
+## Vol. 1 스프링의 이해와 원리
+[1장. 오브젝트와 의존관계](https://moondongmin.notion.site/1-16f357c44e5680d6963ec6d926d88594?pvs=4)
--- chapter01/src/main/java/io/wisoft/tutorial/chapter01/user/dao/DaoFactory.java
+++ chapter01/src/main/java/io/wisoft/tutorial/chapter01/user/dao/DaoFactory.java
@@ -1,18 +1,33 @@
 package io.wisoft.tutorial.chapter01.user.dao;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DaoFactory {
+  Dotenv dotenv = Dotenv.configure().directory("/Users/moon/Developments/graduate-school/tobyspring/chapter01/.env").load();
+
 
   @Bean
   public UserDao userDao() {
-    return new UserDao(connectionMaker());
+    UserDao userDao = new UserDao();
+    userDao.setDataSource(dataSource());
+    return userDao;
   }
 
   @Bean
-  public ConnectionMaker connectionMaker() {
-    return new DConnectionMaker();
+  public DataSource dataSource() {
+    SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+    dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+    dataSource.setUrl("jdbc:mysql://localhost:3306/tobySpring");
+    dataSource.setUsername(dotenv.get("ID"));
+    dataSource.setPassword(dotenv.get("PASSWORD"));
+
+    return dataSource;
   }
 }
